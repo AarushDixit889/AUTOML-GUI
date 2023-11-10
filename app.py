4968ce09687aa08bc9af2a151b31e62d7c594451
Shivansh Dixit <133681987+AarushDixit889@users.noreply.github.com>
2023-11-10 13:55:39 +0530

    ADD ML SELECTION

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ from sklearn.linear_model import LinearRegression,LogisticRegression
 from sklearn.svm import SVR,SVC
 from sklearn.naive_bayes import BernoulliNB,GaussianNB,MultinomialNB
 from sklearn.ensemble import RandomForestRegressor,RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 import joblib
+from pycaret.classification import setup as setup_clf, compare_models as compare_clf,pull as pull_clf,save_model as save_clf,load_model as load_clf,predict_model as predict_clf
+from pycaret.regression import setup as setup_reg, compare_models as compare_reg,pull as pull_reg,save_model as save_reg,load_model as load_reg,predict_model as predict_reg
 from ydata_profiling import ProfileReport
 with st.sidebar:
     st.image("ml.jpeg")
@@ -51,6 +54,12 @@ if choice=="Preprocessing":
             data=pd.get_dummies(data,drop_first=True)
             st.dataframe(data)
             data.to_csv("sourcefile.csv")
+        le_col=st.selectbox("Column you want to encode?",data.columns)
+        if st.button("Label Encoder"):
+            le=LabelEncoder()
+            data[le_col]=le.fit_transform(data[le_col])
+            st.dataframe(le.fit_transform(data[le_col]))
+            data.to_csv("sourcefile.csv")
     else:
         st.error("Firstly upload your file")
 if choice=="ML Model":
@@ -58,55 +67,62 @@ if choice=="ML Model":
         st.header("Select your target and ML Model")
         data=pd.read_csv("sourcefile.csv")
         target=st.selectbox("Target",data.columns)
+        clf_or_reg=st.selectbox("Classification or Regression",['Classification',"Regression"])
         savepath=st.text_input("Where to save model?")
-        model=st.selectbox("Model",['Linear Regression',"Logistic Regression","SVR","SVC","Random Forest Regressor","Random Forest Classifier","Mulltinomial Naive Bayes","Bernoulli Naive Bayes","Gaussian Naive Bayes"])
-        skModels=[LinearRegression(),LogisticRegression(),SVR(),SVC(),RandomForestRegressor(),RandomForestClassifier(),MultinomialNB(),BernoulliNB(),GaussianNB()]
-        X=data.drop(target,axis=1)
-        y=data[target]
-        X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2)
-        if model=="Linear Regression":
-            index=0
-        if model=="Logistic Regression":
-            index=1
-        if model=="SVR":
-            index=2
-        if model=="SVC":
-            index=3
-        if model=="Random Forest Regressor":
-            index=4
-        if model=="Random Forest Classifier":
-            index=5
-        if model=="Mulltinomial Naive Bayes":
-            index=6
-        if model=="Bernoulli Naive Bayes":
-            index=7
-        if model=="Gaussian Naive Bayes":
-            index=8
-        m=skModels[index]
-        m.fit(X_train,y_train)
-        score=m.score(X_test,y_test)
-        st.write("Score:"+str(score))
-        if st.button("Download Model"):
-            joblib.dump(m,savepath)
+        if st.button("Build"):
+            best_model=None
+            if clf_or_reg=="Classification":
+                exp_clf=setup_clf(data,target=target)
+                st.header("Setup")
+                exp_clf=pull_clf()
+                st.dataframe(exp_clf)
+                best_model=compare_clf()
+                model_df=pull_clf()
+                st.header("Models(Ranking Based)")
+                st.dataframe(model_df)
+            elif clf_or_reg=="Regression":
+                exp_reg=setup_reg(data,target=target)
+                st.header("Setup")
+                exp_reg=pull_reg()
+                st.dataframe(exp_reg)
+                best_model=compare_reg()
+                model_df=pull_reg()
+                st.header("Models(Ranking Based)")
+                st.dataframe(model_df)
+            if st.button("Download"):
+                if clf_or_reg=="Classification":
+                    save_clf(best_model,savepath)
+                elif clf_or_reg=="Regression":
+                    save_reg(best_model,savepath)
     else:
         st.error("Source File is not present")
 if choice=="Prediction":
 
-    model=st.text_input("Model's path")
+    model_path=st.text_input("Model's path")
     test=st.file_uploader("File to predict")
-    if (model is not None) and (test is not None):
+    if (model_path is not None) and (test is not None):
         try:
-            m=joblib.load(model,"r+")
-        except:
+            model=None
+            clf_or_reg=st.selectbox("Classification or Regression",['Classification',"Regression"])
+            if clf_or_reg=="Classification":
+                model=load_clf(model_path)
+            elif clf_or_reg=="Regression":
+                model=load_reg(model_path)
+        except Exception as e:
+            print(e)
             st.error("Write correct path")
         data=pd.read_csv(test)
         try:
-             pred=pd.DataFrame(m.predict(data),columns=['Prediction'])
-             st.dataframe(pred)
-             if st.button("Save Predictions"):
-                predpath=st.text_input("Where to save(folder)?")
+            if clf_or_reg=="Classification":
+                pred=pd.DataFrame(predict_clf(model,data),columns=['Prediction'])
+            elif clf_or_reg=="Regression":
+                pred=pd.DataFrame(predict_reg(model,data),columns=['Prediction'])
+            st.dataframe(pred)
+            if st.button("Save Predictions"):
+                predpath=st.text_input("Where to save(?")
                 if predpath is not None:
-                    pred.to_csv(os.path.join(predpath,"prediction.csv"),index=False)
-        except:
+                    pred.to_csv(predpath,index=False)
+        except Exception as e:
+            print(e)
             st.error("Preprocess your data")
 st.sidebar.info("MADE BY AARUSH DIXIT")
